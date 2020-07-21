c838712394c2bf6c4c88f35079ec722b520c0bd8
yyzs <bjf_1@126.com>
2020-07-21 14:13:51 +0800

    完成登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <el-button type="info" @click="logout">推出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      // 清空缓存， 跳转到登录页
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
