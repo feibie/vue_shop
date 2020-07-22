5cbe39e471785b6b4e8eed79475598f79b34b08b
yyzs <bjf_1@126.com>
2020-07-22 15:06:52 +0800

    完成用户列表功能的开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,10 +14,18 @@
       <el-aside :width="isCollapse ? '64px' : '200px'">
         <div class="toggle-button" @click="toggleCollapse">|||</div>
         <!-- 侧边栏菜单区 -->
-        <el-menu background-color="#333744" text-color="#fff" 
-        active-text-color="#ffd04b" unique-opened :collapse="isCollapse" :collapse-transition="false" router>
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409EFF"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          router
+          :default-active="activePath"
+        >
           <!-- 一级菜单 -->
-          <el-submenu :index="item.id + ''" v-for="item in menuList" :key= "item.id">
+          <el-submenu :index="item.id + ''" v-for="item in menuList" :key="item.id">
             <!-- 一级菜单的模板区域 -->
             <template slot="title">
               <!-- 图标 -->
@@ -26,7 +34,12 @@
               <span>{{item.authName}}</span>
             </template>
             <!-- 二级菜单 -->
-            <el-menu-item :index="'/' + subItem.path" v-for="subItem in item.children" :key="subItem.id">
+            <el-menu-item
+              :index="'/' + subItem.path"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+              @click="saveNavState('/' + subItem.path)"
+            >
               <template>
                 <i class="el-icon-menu"></i>
                 <span>{{subItem.authName}}</span>
@@ -50,18 +63,21 @@ export default {
     return {
       menuList: [],
       iconObje: {
-        '125': '',
-        '103': '',
-        '101': '',
-        '102': '',
-        '145': ''
+        125: 'a',
+        103: 'b',
+        101: 'c',
+        102: 'd',
+        145: 'e'
       },
       // 是否折叠
-      isCollapse: false
+      isCollapse: false,
+      // 被激活的链接地址
+      activePath: ''
     }
   },
   created() {
-    this.getMenuList()
+    this.getMenuList(),
+    this.activePath = window.sessionStorage.getItem('activePath')
   },
   methods: {
     logout() {
@@ -71,13 +87,18 @@ export default {
     },
     // 获取所以的菜单
     async getMenuList() {
-      const {data:res} = await this.$http.get('menus')
-      if(res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
       this.menuList = res.data
     },
-    //点击按钮切换菜单的折叠与展开
+    // 点击按钮切换菜单的折叠与展开
     toggleCollapse() {
       this.isCollapse = !this.isCollapse
+    },
+    // 保存链接的激活状态
+    saveNavState(activePath) {
+      window.sessionStorage.setItem('activePath', activePath)
+      this.activePath = activePath
     }
   }
 }
@@ -110,7 +131,7 @@ export default {
 
 .el-aside {
   background-color: #333744;
-  .el-menu{
+  .el-menu {
     border: none;
   }
 }
@@ -119,13 +140,13 @@ export default {
   background-color: #eaedf1;
 }
 
-.toggle-button{
+.toggle-button {
   background-color: #4a5064;
   font-size: 10px;
   line-height: 24px;
-  color:#fff;
+  color: #fff;
   text-align: center;
   letter-spacing: 0.2em;
-  cursor:pointer;
+  cursor: pointer;
 }
 </style>
