82ab8be2ca4168033959198494a9906c043fdfe5
yyzs <bjf_1@126.com>
2020-08-06 09:55:19 +0800

    旺财商品功能的开发

--- src/main.js
+++ src/main.js
@@ -11,6 +11,13 @@ import './assets/css/global.css'
 // 导入
 import TreeTable from 'vue-table-with-tree-grid'
 
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
 // 导入axios
 import axios from 'axios'
 // 配置请求的根路径
@@ -24,6 +31,23 @@ axios.interceptors.request.use(config => {
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
+
+// 将富文本编辑器，注册为全局可用的组件
+/* { default global options } */
+Vue.use(VueQuillEditor)
+
+Vue.filter('dataFormat', function (originVal) {
+  const dt = new Date(originVal)
+
+  const Y = dt.getFullYear()
+  const M = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const D = (dt.getDate() + '').padStart(2, '0')
+  const h = (dt.getHours() + '').padStart(2, '0')
+  const m = (dt.getMinutes() + '').padStart(2, '0')
+  const s = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${Y}-${M}-${D} ${h}:${m}:${s}`
+})
 
 new Vue({
   router,
